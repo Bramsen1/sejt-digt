9f526f8ffe997f40acbdb87661a948dc9f2147f6
Jakob <jakobwjensen@gmail.com>
2026-09-18 14:23:05 +0200

    Tilføjet hej viktor

--- digt laver.py
+++ digt laver.py
@@ -1,13 +1,8 @@
 print("Regndråber trommer mod ruden nu,")
+print("kaffen er varm og koppen er blå.")
 print("Verden suser afsted i et nu,")
 print("men lige her kan vi trygt stå.")
 print("Et lille smil i det fjerne lys,")
 print("glemmer for altid vinterens gys.")
 print("Dagen åbner sin stille favn,")
-print("og hvisker blidt dit eget navn.")
-
-
-
-
-
-
+print("Hej Viktor :)")
