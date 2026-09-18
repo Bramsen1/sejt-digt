d47b82b1eeeeaa18369cb847a279852b7a1da440
viktor <zbc25vpbr@zbc.dk>
2026-09-18 14:04:26 +0200

    Har udvidet digtet med to linjer

--- digt laver.py
+++ digt laver.py
@@ -4,6 +4,8 @@ print("Verden suser afsted i et nu,")
 print("men lige her kan vi trygt stå.")
 print("Et lille smil i det fjerne lys,")
 print("glemmer for altid vinterens gys.")
+print("Dagen åbner sin stille favn,")
+print("og hvisker blidt dit eget navn.")
 
 
 
